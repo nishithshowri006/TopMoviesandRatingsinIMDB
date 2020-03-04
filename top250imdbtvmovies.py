16e33aa59f161e4797becba80bdf3a11423b5a79
Nishith Showri <58651995+nishithshowri006@users.noreply.github.com>
2020-03-05 01:32:17 +0530

    Update top250imdbtvmovies.py

--- top250imdbtvmovies.py
+++ top250imdbtvmovies.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 def movies(filename,path):
    
-    os.chdir(filename)
+    os.chdir(path)
     connect=requests.get(""" https://www.imdb.com/chart/top/?ref_=nv_mv_250 """)
     soup=BeautifulSoup(connect.text,'html.parser')
     elements_title=soup.find_all('td',attrs='titleColumn')
